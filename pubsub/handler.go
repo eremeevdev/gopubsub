341c0a42353650dfd49f9b0955b57c339ac94e5f
Aleksey Eremeev <eremeev.dev@gmail.com>
2017-01-15 15:00:30 +0300

    parse commands

--- pubsub/handler.go
+++ pubsub/handler.go
@@ -1,9 +1,44 @@
 package pubsub
 
-import "net"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+)
 
 // Handler - handle new client connections
 func Handler(conn net.Conn) {
-	conn.Write([]byte("ok\n"))
-	conn.Close()
+
+	reader := bufio.NewReader(conn)
+
+	data, err := reader.ReadString('\n')
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	command := strings.Split(strings.TrimSuffix(data, "\n"), " ")
+
+	switch command[0] {
+
+	case "PUBLISH":
+		go Publish(conn, command)
+
+	case "SUBSCRIBE":
+		go Subscribe(conn, command)
+	}
+
+}
+
+// Publish - handle pub command
+func Publish(conn net.Conn, command []string) {
+	defer conn.Close()
+	fmt.Printf("PUBLISH %v TO: %v\n", command[1], command[2:])
+}
+
+// Subscribe - handle subscribe command
+func Subscribe(conn net.Conn, command []string) {
+	fmt.Println("SUBSCRIBE TO:", command[1:])
 }
